c34cee94021aefd00b4ecd945f35217ce5402528
John Miller <John.Miller@neudesic.com>
2018-10-01 18:04:37 -0700

    Code reformatting and minor changes

--- CertifiedProfessionalDeveloper.2018/ALB/acg.alb.sh
+++ CertifiedProfessionalDeveloper.2018/ALB/acg.alb.sh
@@ -13,28 +13,53 @@ aws configure set default.region $region
 mkdir -p ~/.ssh
 keyName="ec2-alb-$region"
 echo $keyName
-aws ec2 create-key-pair --key-name $keyName | jq -r ".KeyMaterial" > ~/.ssh/$keyName.pem
+
+# WIP to avoid error when key exists
+#if [$(aws ec2 describe-key-pairs --filter  'Name=key-name,Values=$keyName' | jq '.KeyPairs | length==0')]
+  aws ec2 create-key-pair \
+    --key-name $keyName \
+    | jq -r ".KeyMaterial" > ~/.ssh/$keyName.pem
+#fi
 
 chmod 400 ~/.ssh/$keyName.pem
 
 # Create a security group 
-securityGroupId=$(aws ec2 create-security-group --description "This ALB SG allows traffic only on port 80 and port 22" --group-name "ALBWebDMZ" | jq -r .GroupId)
+securityGroupId=$(aws ec2 create-security-group \
+  --description "This ALB SG allows traffic only on port 80 and port 22" \
+  --group-name "ALBWebDMZ" \
+  | jq -r .GroupId)
 echo $securityGroupId
 
 myIp=$(dig +short myip.opendns.com @resolver1.opendns.com)
 myCidr="$myIp/32"
 allCidr="0.0.0.0/0"
 
-aws ec2 authorize-security-group-ingress --group-id $securityGroupId --protocol tcp --port 22 --cidr $myCidr
-aws ec2 authorize-security-group-ingress --group-id $securityGroupId --protocol tcp --port 80 --cidr $allCidr
-aws ec2 describe-security-groups  --group-ids $securityGroupId | jq -r ".SecurityGroups[].GroupId"
+aws ec2 authorize-security-group-ingress \
+  --group-id $securityGroupId \
+  --protocol tcp \
+  --port 22 \
+  --cidr $myCidr
+aws ec2 authorize-security-group-ingress \
+  --group-id $securityGroupId \
+  --protocol tcp \
+  --port 80 \
+  --cidr $allCidr
+aws ec2 describe-security-groups 
+  --group-ids $securityGroupId \
+  | jq -r ".SecurityGroups[].GroupId"
 
 # Get VPC and Subnet Ids
-vpcId=$(aws ec2 describe-vpcs --filter "Name=isDefault,Values=true" | jq -r ".Vpcs | .[] | .VpcId")
+vpcId=$(aws ec2 describe-vpcs \
+  --filter "Name=isDefault,Values=true" \
+  | jq -r ".Vpcs | .[] | .VpcId")
 echo $vpcId
 
-subnet1=$(aws ec2 describe-subnets --filter "Name=vpc-id,Values=$vpcId" | jq -r ".Subnets | .[0] | .SubnetId")
-subnet2=$(aws ec2 describe-subnets --filter "Name=vpc-id,Values=$vpcId" | jq -r ".Subnets | .[1] | .SubnetId")
+subnet1=$(aws ec2 describe-subnets \
+  --filter "Name=vpc-id,Values=$vpcId" \
+  | jq -r ".Subnets | .[0] | .SubnetId")
+subnet2=$(aws ec2 describe-subnets \
+  --filter "Name=vpc-id,Values=$vpcId" \
+  | jq -r ".Subnets | .[1] | .SubnetId")
 
 echo $subnet1, $subnet2
 
@@ -44,7 +69,11 @@ echo $keyName, $securityGroupId, $myIp, $myCidr, $vpcId, $subnet1, $subnet2
 # Create a couple of instances to load balance
 
 # Get the default Linux AMI for the current region
-ami=$(aws ec2 describe-images --filters "Name=description,Values=Amazon Linux 2 AMI*" "Name=architecture,Values=x86_64" "Name=block-device-mapping.volume-type,Values=gp2"| jq -r '.Images | .[0] | .ImageId')
+ami=$(aws ec2 describe-images \
+  --filters "Name=description,Values=Amazon Linux 2 AMI*" \
+    "Name=architecture,Values=x86_64" \
+    "Name=block-device-mapping.volume-type,Values=gp2" \
+    | jq -r '.Images | .[0] | .ImageId')
 echo $ami
 
 instanceType="t2.micro"
@@ -72,48 +101,85 @@ echo "$userData"
 
 # Placement
 availabilityZones=$(aws ec2 describe-availability-zones)
-availabilityZone=$(echo "$availabilityZones" | jq -r ".AvailabilityZones[0].ZoneName")
+availabilityZone=$(echo "$availabilityZones" \
+  | jq -r ".AvailabilityZones[0].ZoneName")
 echo $availabilityZone
 
 placement="AvailabilityZone=$availabilityZone,Tenancy=default"
 echo $placement
 
 # Launch 2 default linux instances in the default vpc
-instances=$(aws ec2 run-instances --image-id $ami --count 2 --key-name $keyName --security-group-ids $securityGroupId --associate-public-ip-address --instance-type $instanceType --user-data "$userData" --placement $placement)
+instances=$(aws ec2 run-instances \
+  --image-id $ami \
+  --count 2 \
+  --key-name $keyName \
+  --security-group-ids $securityGroupId \
+  --associate-public-ip-address \
+  --instance-type $instanceType \
+  --user-data "$userData" \
+  --placement $placement \
+  )
 
-instanceId1=$(echo $instances | jq -r '.Instances | .[0] | .InstanceId')
-instanceId2=$(echo $instances | jq -r '.Instances | .[1] | .InstanceId')
+instanceId1=$(echo $instances \
+  | jq -r '.Instances | .[0] | .InstanceId')
+instanceId2=$(echo $instances \
+  | jq -r '.Instances | .[1] | .InstanceId')
 
 echo $instanceId1, $instanceId2
 
-aws ec2 wait instance-running --instance-ids $instanceId1 $instanceId2
+aws ec2 wait instance-running \
+  --instance-ids $instanceId1 $instanceId2
 
-aws ec2 describe-instances --instance-ids $instanceId1 $instanceId2 | jq -r ".Reservations[].Instances[].State"
+aws ec2 describe-instances \
+  --instance-ids $instanceId1 $instanceId2 \
+  | jq -r ".Reservations[].Instances[].State"
 
-publicDNS1=$(aws ec2 describe-instances --instance-ids $instanceId1 $instanceId2 | jq -r ".Reservations[].Instances[0].NetworkInterfaces[].Association.PublicDnsName")
-publicDNS2=$(aws ec2 describe-instances --instance-ids $instanceId1 $instanceId2 | jq -r ".Reservations[].Instances[0].NetworkInterfaces[].Association.PublicDnsName")
+publicDNS1=$(aws ec2 describe-instances \
+  --instance-ids $instanceId1 $instanceId2 \
+  | jq -r ".Reservations[].Instances[0].NetworkInterfaces[].Association.PublicDnsName")
+publicDNS2=$(aws ec2 describe-instances \
+  --instance-ids $instanceId1 $instanceId2 \
+  | jq -r ".Reservations[].Instances[0].NetworkInterfaces[].Association.PublicDnsName")
 
 echo $publicDNS1, $publicDNS2
 
 curl $publicDNS1
 curl $publicDNS2
 
-loadBalancer=$(aws elbv2 create-load-balancer --name my-application-load-balancer  --subnets $subnet1 $subnet2 --security-groups $securityGroupId)
+loadBalancer=$(aws elbv2 create-load-balancer \
+  --name my-application-load-balancer \
+  --subnets $subnet1 $subnet2 \
+  --security-groups $securityGroupId)
 
-loadBalancerArn=$(echo "$loadBalancer" | jq -r ".LoadBalancers[].LoadBalancerArn")
+loadBalancerArn=$(echo "$loadBalancer" \
+  | jq -r ".LoadBalancers[].LoadBalancerArn")
 echo $loadBalancerArn
-loadBalancerDns=$(echo "$loadBalancer" | jq -r ".LoadBalancers[].DNSName")
+loadBalancerDns=$(echo "$loadBalancer" \
+  | jq -r ".LoadBalancers[].DNSName")
 echo $loadBalancerDns
 
-targetGroupArn=$(aws elbv2 create-target-group --name my-targets --protocol HTTP --port 80 --vpc-id $vpcId | jq -r ".TargetGroups[].TargetGroupArn")
+targetGroupArn=$(aws elbv2 create-target-group \
+  --name my-targets \
+  --protocol HTTP \
+  --port 80 \
+  --vpc-id $vpcId \
+  | jq -r ".TargetGroups[].TargetGroupArn")
 echo $targetGroupArn
 
-aws elbv2 register-targets --target-group-arn $targetGroupArn --targets Id=$instanceId1 Id=$instanceId2
+aws elbv2 register-targets \
+  --target-group-arn $targetGroupArn \
+  --targets Id=$instanceId1 Id=$instanceId2
 
-listenerArn=$(aws elbv2 create-listener --load-balancer-arn $loadBalancerArn --protocol HTTP --port 80 --default-actions Type=forward,TargetGroupArn=$targetGroupArn  | jq -r ".Listeners[].ListenerArn")
+listenerArn=$(aws elbv2 create-listener \
+  --load-balancer-arn $loadBalancerArn \
+  --protocol HTTP \
+  --port 80 \
+  --default-actions Type=forward,TargetGroupArn=$targetGroupArn 
+  | jq -r ".Listeners[].ListenerArn")
 echo "$listenerArn"
 
-aws elbv2 describe-target-health --target-group-arn $targetGroupArn
+aws elbv2 describe-target-health \
+  --target-group-arn $targetGroupArn
 
 curl $loadBalancerDns
 
@@ -121,16 +187,22 @@ curl $loadBalancerDns
 ## Cleanup
 
 # Delete load balancer
-aws elbv2 delete-load-balancer --load-balancer-arn $loadBalancerArn
-aws elbv2 delete-target-group --target-group-arn $targetGroupArn
+aws elbv2 delete-load-balancer \
+  --load-balancer-arn $loadBalancerArn
+aws elbv2 delete-target-group \
+  --target-group-arn $targetGroupArn
 
 # Terminate the instances
-aws ec2 terminate-instances --instance-ids $instanceId1 $instanceId2 | jq -r '.TerminatingInstances | .[] | .InstanceId, .PreviousState, .CurrentState'
+aws ec2 terminate-instances \
+  --instance-ids $instanceId1 $instanceId2 \
+  | jq -r '.TerminatingInstances | .[] | .InstanceId, .PreviousState, .CurrentState'
 
 # Delete key pair
-aws ec2 delete-key-pair --key-name $keyName
+aws ec2 delete-key-pair \
+  --key-name $keyName
 rm ~/.ssh/$keyName.pem -f
 ls ~/.ssh
 
 # Delete the security group
-aws ec2 delete-security-group --group-id $securityGroupId 
+aws ec2 delete-security-group \
+  --group-id $securityGroupId 
--- CertifiedProfessionalDeveloper.2018/EC2-101/acg.ec2-101.sh
+++ CertifiedProfessionalDeveloper.2018/EC2-101/acg.ec2-101.sh
@@ -7,19 +7,28 @@
 
 ## Create a security group
 
-# Computation
+# get external ip and cidr
 myIp=$(dig +short myip.opendns.com @resolver1.opendns.com)
 myCidr="$myIp/32"
 
-# Initialization
+# create the security group
 securityGroupId=$(aws ec2 create-security-group --description "This SG allows traffic from the current external ip on port 80 and port 22 " --group-name "WebDMZ" | jq -r .GroupId)
 
-# Implementation
-aws ec2 authorize-security-group-ingress --group-id $securityGroupId --protocol tcp --port 22 --cidr $myCidr
-aws ec2 authorize-security-group-ingress --group-id $securityGroupId --protocol tcp --port 80 --cidr $myCidr
+# open ports for ssh and http
+aws ec2 authorize-security-group-ingress \
+  --group-id $securityGroupId \
+  --protocol tcp \
+  --port 22 \
+  --cidr $myCidr
+aws ec2 authorize-security-group-ingress \
+  --group-id $securityGroupId \
+  --protocol tcp \
+  --port 80 \
+  --cidr $myCidr
 
-# Assertation
-aws ec2 describe-security-groups  --group-ids $securityGroupId
+# Show that the sg was created
+aws ec2 describe-security-groups \
+--group-ids $securityGroupId
 
 
 ## EC2 Create, describe and terminate default Linux instance in the default region
@@ -28,28 +37,45 @@ aws ec2 describe-security-groups  --group-ids $securityGroupId
 mkdir -p ~/.ssh
 keyName='EC2.101'
 #!: Make idempotent
-aws ec2 create-key-pair --key-name $keyName | jq -r ".KeyMaterial" > ~/.ssh/$keyName.pem
+aws ec2 create-key-pair --key-name $keyName \
+  | jq -r ".KeyMaterial" > ~/.ssh/$keyName.pem
 chmod 400 ~/.ssh/$keyName.pem
 
 # Get the default Linux AMI for the current region
-ami=$(aws ec2 describe-images --filters "Name=description,Values=Amazon Linux 2 AMI*" "Name=architecture,Values=x86_64" "Name=block-device-mapping.volume-type,Values=gp2"| jq -r '.Images | .[0] | .ImageId')
+ami=$(aws ec2 describe-images \
+  --filters "Name=description,Values=Amazon Linux 2 AMI*" \
+    "Name=architecture,Values=x86_64" \
+    "Name=block-device-mapping.volume-type,Values=gp2" \
+    | jq -r '.Images | .[0] | .ImageId')
 echo $ami
 
 instanceType="t2.micro"
 
 # Launch 1 default linux l2 instance in the default vpc
-instanceId=$(aws ec2 run-instances --image-id $ami --count 1 --key-name $keyName --security-group-ids $securityGroupId --associate-public-ip-address --instance-type $instanceType | jq -r '.Instances | .[0] | .InstanceId')
+instanceId=$(aws ec2 run-instances \
+  --image-id $ami \
+  --count 1 \
+  --key-name $keyName \
+  --security-group-ids $securityGroupId \
+  --associate-public-ip-address \
+  --instance-type $instanceType \
+  | jq -r '.Instances | .[0] | .InstanceId')
 # Assertion
 echo $instanceId
 
 # Get the instance states
 #!: Wait before proceeding
-aws ec2 wait instance-running --instance-ids $instanceId
-aws ec2 describe-instances --instance-ids $instanceId | jq -r '.Reservations | .[] | .Instances | .[] | .InstanceId,.State'
-
+aws ec2 wait instance-running \
+  --instance-ids $instanceId
+aws ec2 describe-instances \
+  --instance-ids $instanceId \
+  | jq -r '.Reservations | .[] | .Instances | .[] | .InstanceId,.State'
 
 # logon to instance
-publicDNS=$(aws ec2 describe-instances --instance-ids $instanceId | jq -r '.Reservations | .[] | .Instances | .[] | .NetworkInterfaces | .[] | .Association.PublicDnsName')
+publicDNS=$(aws ec2 describe-instances \
+  --instance-ids $instanceId \
+  | jq -r '.Reservations | .[] | .Instances | .[] | .NetworkInterfaces | .[] | .Association.PublicDnsName')
+
 # Assertion
 echo $publicDNS
 
@@ -83,11 +109,15 @@ curl $publicDNS
 ## Clean-up 
 
 # Terminate the instances
-aws ec2 terminate-instances --instance-ids $instanceId | jq -r '.TerminatingInstances | .[] | .InstanceId, .PreviousState, .CurrentState'
+aws ec2 terminate-instances \
+  --instance-ids $instanceId \
+  | jq -r '.TerminatingInstances | .[] | .InstanceId, .PreviousState, .CurrentState'
 
 # Delete key pair
-aws ec2 delete-key-pair --key-name $keyName
+aws ec2 delete-key-pair \
+  --key-name $keyName
 rm ~/.ssh/$keyName.pem -f
 
 # Delete the security group
-aws ec2 delete-security-group --group-id $securityGroupId 
+aws ec2 delete-security-group \
+  --group-id $securityGroupId 
